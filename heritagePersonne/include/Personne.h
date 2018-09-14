0940f08282df1a19ff581b85019cda40debf771b
gnoumy <gaucher.ju@gmail.com>
2018-09-14 14:53:45 +0200

    Exercice -> Ajout polymorphisme

--- heritagePersonne/include/Personne.h
+++ heritagePersonne/include/Personne.h
@@ -17,7 +17,9 @@ class Personne
 
     public:
         Personne(string pNom, string pPrenom, int pAge);
+        Personne() = default; //autorise la création d'un objet avec un constructeur par défaut
         virtual ~Personne();
+        virtual string info(); // Polymorphisme de la fonction info
 
         void setNom(string pNom);
         void setPrenom(string pPrenom);
@@ -27,7 +29,7 @@ class Personne
         string getPrenom(){return this->prenom;};
         int getAge(){return this->age;};
 
-        string info();
+        //string info();
         string getInitials();
 
         static void ChangeMajorite(int);
--- heritagePersonne/main.cpp
+++ heritagePersonne/main.cpp
@@ -11,6 +11,8 @@ int main()
     Personne p2("Martin", "Steve", 12);
     Salarie  s("Martin", "Steve", 44, 20010);
     /********* FIN Instanciation *********/
+
+
     cout<<"----- Personne -----" <<endl;
     cout<<p.info() << p.getInitials() <<endl;
 
@@ -20,5 +22,32 @@ int main()
     cout<< endl << "----- Salarie -----" <<endl;
     cout<<s.info();
 
+
+    /************************************************************/
+    Personne mesPers[3];
+
+    mesPers[0] = p;
+    mesPers[1] = p2;
+    mesPers[2] = s;
+
+    for(int i=0; i<3; i++)
+    {
+        cout << mesPers[i].info() << endl;
+        // appellera la méthode infos de Personne et n'affichera pas le salaire
+        // car la méthode info() n'est pas déclarée en tant que VIRTUAL
+    }
+    /************************************************************/
+
+    /************************************************************/
+    Personne * personnel[3];
+    personnel[0] = new Personne("Delouvrier", "Romain", 26);
+    personnel[1] = new Salarie("Delouvrier", "Romain", 26, 23000);
+    personnel[2] = new Personne("Delouvrier", "Romain", 26);
+    for(int i=0; i<3; i++)
+    {
+        cout << personnel[i]->info() << endl;
+        //personnel est un pointeur, donc on utilise la flèche ->
+    }
+    /************************************************************/
     return 0;
 }
